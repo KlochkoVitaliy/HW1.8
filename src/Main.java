aefd5b9c67732a51057a33d0f3e3388670419dc8
VitaliyKlochko <89vklochko89@gmail.com>
2022-08-22 22:15:45 +0300

    Begin+

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-1
+        int year = 2024;
+        boolean yearVis = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+        if(yearVis){
+            System.out.println("Год является високосным");
+        }else {
+            System.out.println("Год не является високосным");
+        }
     }
 }
